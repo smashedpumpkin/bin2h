91954708a0f2cd86be3f98b07383af12691fdeae
Matt Gordon <matt@Matts-MacBook-Air.local>
2020-04-25 01:02:16 -0700

    add cstdarg header for windows build

--- bin2h.cpp
+++ bin2h.cpp
@@ -24,6 +24,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cstdarg>
 
 //prints help to stdout
 void Help()
